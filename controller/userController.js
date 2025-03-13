879dea5b51928ac8f60e8c9872c0ede63cdf24ac
Vishnu palanivel M <mvishnupalanivel@gmail.com>
2025-03-13 16:14:01 +0530

    Update userController.js

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,5 @@
 const asyncHandler = require('express-async-handler');
-const Usernew = require('../models/userSchema');
+const Usernew = require('../models/UserSchema ');
 const nodemailer = require("nodemailer"); // ✅ Import nodemailer
 const axios = require("axios");
 const OTP_STORE = {}; 
