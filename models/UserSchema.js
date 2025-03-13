3b0316f6a8edab4dd22456a0fed3119e6e1a21b5
Vishnu palanivel M <mvishnupalanivel@gmail.com>
2025-03-13 14:42:20 +0530

    Create UserSchema.js

---
+++ models/UserSchema.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const UserSchema = mongoose.Schema({
+    phone:{
+        type: String,
+        required: [true, 'Please enter proper phone'],
+    },
+    name:{
+        type: String,
+        required: [true, 'Please enter proper name'],
+    },
+    otp:{
+        type: String,
+        required: [true, 'Please enter proper otp'],
+    },
+    email: { 
+        type: String, 
+        unique: true,
+        default: function() { return `user_${Date.now()}@example.com`; } // Assigns a unique default email
+    },
+    ImageBase64:{
+        type: String,
+        required: [true, 'Please enter proper images'],
+    },
+    createdAt: { type: Date, default: Date.now } // Default timestamp
+
+    
+});
+
+module.exports = mongoose.model('users', UserSchema);
